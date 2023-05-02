a22d516413a51303aa599e87b4a127b6599a0390
Shreyas <shreyas.misra03@gmail.com>
2023-05-02 10:51:30 +0530

    Minor change

--- pages/projects.js
+++ pages/projects.js
@@ -8,14 +8,14 @@ import React from "react";
 import { motion } from "framer-motion";
 import TransitionsEffects from "@/src/components/TransitionsEffects";
 
-// import TheSocialScoop from "../public/images/TheSocialScoop.png";
+import TheSocialScoop from "../public/images/thesocialscoop.png";
 // import Gericht from "../public/images/Gericht.png";
 // import ShowFinder from "../public/images/ShowFinder.png";
 // import BloggerJs from "../public/images/BloggerJs.png";
 // import TrakerTask from "../public/images/TrakerTask.png";
 // import Connecting from "../public/images/Connecting.png";
 
-const FramerImage = motion(Image);
+// const FramerImage = motion(Image);
 
 const FeauredProject = ({ type, title, img, summary, link, github }) => {
   return (
@@ -35,9 +35,9 @@ const FeauredProject = ({ type, title, img, summary, link, github }) => {
         target="_blank"
         className="w-1/2 cursor-pointer overflow-hidden rounded-2xl lg:w-full"
       >
-        <FramerImage
-          whileHover={{ scale: 1.05 }}
-          transition={{ duration: 0.2 }}
+        <Image
+          // whileHover={{ scale: 1.05 }}
+          // transition={{ duration: 0.2 }}
           src={img}
           alt={title}
           className="w-full h-auto"
@@ -101,9 +101,9 @@ const Project = ({ title, img, github, link }) => {
         target="_blank"
         className="w-full cursor-pointer overflow-hidden rounded-2xl"
       >
-        <FramerImage
-          whileHover={{ scale: 1.05 }}
-          transition={{ duration: 0.2 }}
+        <Image
+          // whileHover={{ scale: 1.05 }}
+          // transition={{ duration: 0.2 }}
           src={img}
           alt={title}
           className="w-full h-auto"
@@ -162,7 +162,7 @@ const projects = () => {
               <FeauredProject
                 type="Featured Project"
                 title={`The Social Scoop`}
-                img="../public/images/thesocialscoop.png"
+                img={TheSocialScoop}
                 summary={
                   "This is a Modern Social Media Web Application which provides all te nessasary funtions of a soical media media application. With a minimal design, has a option for 'Dark' & 'Light' Themes. It use the timeline for fetching posts of the users followers. The passwords & messages are been enceypted."
                 }
