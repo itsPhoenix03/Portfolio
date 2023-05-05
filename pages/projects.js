7ff195d2cc44845ae603d7089f75d6a2c49f833c
Shreyas <shreyas.misra03@gmail.com>
2023-05-05 12:01:15 +0530

    Minor Change

--- pages/projects.js
+++ pages/projects.js
@@ -40,7 +40,7 @@ const FeauredProject = ({ type, title, img, summary, link, github }) => {
           height={500}
           // whileHover={{ scale: 1.05 }}
           // transition={{ duration: 0.2 }}
-          src={img}
+          src={TheSocialScoop}
           alt={title}
           className="w-full h-auto"
           priority
