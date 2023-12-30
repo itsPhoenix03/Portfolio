d5d6ab29b2c2397ff00df77619dcd38952675cad
Shreyas <shreyas.misra03@gmail.com>
2023-12-30 19:00:09 +0530

    Bug Fixed - Production bug fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,8 @@ import Link from "next/link";
 import React, { useState } from "react";
 import Logo from "./Logo";
 import { useRouter } from "next/router";
-import GmailIcon, {
+import {
+  GmailIcon,
   GithubIcon,
   LinkedInIcon,
   MoonIcon,
