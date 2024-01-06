fd58257f44847479fb17c6846b9373a51e711113
Shreyas <shreyas.misra03@gmail.com>
2024-01-06 12:39:46 +0530

    UI Update - Changes made in skills section of about me page for mobile screens

--- src/components/AboutSectionSkills.jsx
+++ src/components/AboutSectionSkills.jsx
@@ -31,11 +31,27 @@ const Skill = ({ name, icon: Icon, x, y }) => {
       cursor-default lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent xs:dark:bg-transparent xs:text-dark xs:dark:text-light xs:font-bold"
       whileHover={{ scale: 1.05 }}
       initial={{ x: 0, y: 0 }}
-      whileInView={{ x, y, transition: { duration: 1.5 } }}
+      whileInView={{ x, y, transition: { type: "spring", duration: 1.5 } }}
     >
       {((Icon && name) || (Icon && !name)) && (
         <Icon className="sm:w-8 sm:h-8" />
       )}
+    </motion.div>
+  );
+};
+
+const SkillMobileView = ({ name, icon: Icon, x, y }) => {
+  return (
+    <motion.div
+      className="absolute flex items-center justify-center py-3 px-6"
+      initial={{ x: 0, y: 0 }}
+      whileInView={{
+        x,
+        y,
+        transition: { type: "spring", duration: 1.5, stiffness: 50 },
+      }}
+    >
+      {((Icon && name) || (Icon && !name)) && <Icon className="w-fit h-fit" />}
     </motion.div>
   );
 };
@@ -49,7 +65,7 @@ const AboutSectionSkills = () => {
 
       <div
         className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark
-      lg:h-[80vh] sm:h-[60vh] xs:h-[50vh]
+      lg:h-[80vh] sm:hidden xs:h-[50vh]
       lg:bg-circularLightLg dark:lg:bg-circularDarkLg
       md:bg-circularLightMd dark:md:bg-circularDarkMd
       sm:bg-circularLightSm dark:sm:bg-circularDarkSm
@@ -97,6 +113,78 @@ const AboutSectionSkills = () => {
           name="Styled Component"
           x="16vw"
           y="-4vw"
+        />
+      </div>
+
+      {/* Mobile View */}
+      <div className="hidden w-full h-screen relative sm:flex items-center justify-center mt-14">
+        <motion.div className="flex items-center justify-center rounded-full font-semibold text-sm bg-dark text-light dark:bg-light dark:text-dark p-4 shadow-dark dark:shadow-light">
+          <Link href="/skills">Web</Link>
+        </motion.div>
+
+        <SkillMobileView icon={ReactIcon} name="ReactJs" x="-30vw" y="10vw" />
+        <SkillMobileView
+          icon={JavaScriptIcon}
+          name="JavaScript"
+          x="-22vw"
+          y="-20vw"
+        />
+        <SkillMobileView
+          icon={TypeScriptIcon}
+          name="TypeScript"
+          x="20vw"
+          y="23vw"
+        />
+        <SkillMobileView icon={NextJsIcon} name="NextJs" x="25vw" y="52vw" />
+        <SkillMobileView icon={CSSIcon} name="CSS" x="-29vw" y="-40vw" />
+        <SkillMobileView
+          icon={TailwindCSSIcon}
+          name="Tailwind CSS"
+          x="34vw"
+          y="-47vw"
+        />
+        <SkillMobileView icon={HtmlIcon} name="HTML" x="0vw" y="78vw" />
+        <SkillMobileView icon={MongoDbIcon} name="MongoDb" x="18vw" y="-68vw" />
+        <SkillMobileView
+          icon={MongooseIcon}
+          name="Mongoose"
+          x="13vw"
+          y="-32vw"
+        />
+        <SkillMobileView
+          icon={ExpressJsIcon}
+          name="ExpressJs"
+          x="-2vw"
+          y="-80vw"
+        />
+        <SkillMobileView icon={NodeJsIcon} name="NodeJs" x="-13vw" y="46vw" />
+        <SkillMobileView icon={ReduxIcon} name="Redux" x="-9vw" y="-50vw" />
+        <SkillMobileView
+          icon={FirebaseIcon}
+          name="Firebase"
+          x="26vw"
+          y="-88vw"
+        />
+        <SkillMobileView icon={NpmIcon} name="Npm" x="-28vw" y="80vw" />
+        <SkillMobileView icon={FigmaIcon} name="Figma" x="32vw" y="-17vw" />
+        <SkillMobileView
+          icon={ReactRouterDomIcon}
+          name="React Router Dom"
+          x="32vw"
+          y="86vw"
+        />
+        <SkillMobileView
+          icon={FramerMotionIcon}
+          name="Framer Motion"
+          x="-32vw"
+          y="-83vw"
+        />
+        <SkillMobileView icon={PrismaIcon} name="Prisma" x="-18vw" y="98vw" />
+        <SkillMobileView
+          icon={StyledComponentIcon}
+          name="Styled Component"
+          x="-22vw"
+          y="32vw"
         />
       </div>
     </>
